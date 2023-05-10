8a89d5ed8cc17b65616140f36e36e9e4fa6ce81a
ABCDabcd022 <132382790+ABCDabcd022@users.noreply.github.com>
2023-05-10 15:20:08 +0300

    Add files via upload

---
+++ TSet.h
@@ -0,0 +1,27 @@
+#include "TBitField.h"
+#pragma once
+class TSet
+{
+	int MaxPower;
+	TBitField BitField;
+public:
+	TSet(int n);
+	TSet(const TSet& s);
+	TSet(const TBitField& bf);
+	operator TBitField();
+	void InsElem(const int Elem);
+	void DelElem(const int Elem);
+	int IsMember(const int Elem);
+
+	int operator == (const TSet& s);
+	TSet& operator = (const TSet& s);
+	TSet operator + (int Elem);
+	TSet operator - (int Elem);
+	TSet operator + (const TSet& s);
+	TSet operator * (const TSet& s);
+	TSet operator~();
+
+	friend ostream& operator << (ostream& os, const TSet& s);
+	friend std::istream& operator>>(istream& stream, TSet& s);
+};
+
